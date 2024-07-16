ba849055ec3dd8447261c801c0e66ca6084404a6
fanxin_sun <fanxin_sun@apple.com>
2024-07-16 18:06:24 +0800

    2nd

--- TCP_server.cpp
+++ TCP_server.cpp
@@ -6,6 +6,9 @@
 #include <string>
 #include <algorithm>
 
+#define TCP_PORT 8080
+#define UDP_PORT 9090
+
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
@@ -13,21 +16,19 @@ int main() {
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
 
-    // Create socket file descriptor:
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0))  == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080:
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
-    address.sin_family = AF_INET;
+    address.sin_family      = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
+    address.sin_port        = htons(8080);
 
     // Binding:
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
@@ -35,34 +36,44 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    std::cout << "==================================\n          Server Started:\n==================================" << std::endl;
-
     // Listen:
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    // Accept a connection:
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+    // Accept connection from UDP:
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
+    // Create socket for UDP:
+    int udp_sockfd;
+    struct sockaddr_in udp_servaddr;
+
+    if ((udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("UDP socket craetion failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&udp_servaddr, 0, sizeof(udp_servaddr));
+    udp_servaddr.sin_family      = AF_INET;
+    udp_servaddr.sin_port        = htons(UDP_PORT);
+    udp_servaddr.sin_addr.s_addr = INADDR_ANY;
+
     while(true) {
-        memset(buffer, 0, sizeof(buffer));  // clear the buffer;
+        memset(buffer, 0, sizeof(buffer));
         int valread = read(new_socket, buffer, 1024);
-        if (valread <= 0) {
-            break;
-        }
-        std::cout << "Command received: \n" << buffer << std::endl;
+        if (valread <= 0)   break;
+        std::cout << "Data received: " << buffer << std::endl;
 
-        // Convert buffer to uppercase for comparison
-        std::string command(buffer);
-        std::transform(command.begin(), command.end(), command.begin(), ::toupper);
+        // Forward data to UDP server:
+        sendto(udp_sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *)&udp_servaddr, sizeof(udp_servaddr));
+        std::cout << "Forwarded to UDP" << std::endl;
 
-        // "QUIT" makes it quit:
-        if (command == "QUIT") {
+        // "QUIT" to quit the program:
+        if (strcmp(buffer, "quit") == 0) {
             std::cout << "Received QUIT command. Stopping server." << std::endl;
             break;
         }
@@ -70,6 +81,7 @@ int main() {
 
     close(new_socket);
     close(server_fd);
+    close(udp_sockfd);
 
     return 0;
 }
--- UDP_server.cpp
+++ UDP_server.cpp
@@ -5,7 +5,8 @@
 #include <cstring>
 #include <algorithm>
 
-#define PORT 8080
+// #define PORT 8080
+#define UDP_PORT 9090
 
 int main() {
     int sockfd;                         // Socket File Descriptor;
@@ -22,7 +23,7 @@ int main() {
     
     servaddr.sin_family      = AF_INET; // IPv4
     servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port        = htons(PORT);
+    servaddr.sin_port        = htons(UDP_PORT);
     
     if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("binding failed");
@@ -35,28 +36,28 @@ int main() {
     
     len = sizeof(cliaddr); // len = value/result
     
-    std::cout << "==================================\n          Server Started:\n==================================" << std::endl;
+    std::cout << "==================================\n         UDP Server Started:\n==================================" << std::endl;
 
     while (true) {
         memset(buffer, 0, sizeof(buffer));  // clear the buffer;
         
         n = recvfrom(sockfd, (char *)buffer, 1024, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
         buffer[n] = '\0';
-        std::cout << "Client: " << buffer << std::endl;
+        std::cout << "Received from TCP: " << buffer << std::endl;
         
-        // Convert buffer to uppercase for comparison
-        std::string command(buffer);
-        std::transform(command.begin(), command.end(), command.begin(), ::toupper);
+        // // Convert buffer to uppercase for comparison
+        // std::string command(buffer);
+        // std::transform(command.begin(), command.end(), command.begin(), ::toupper);
         
         // Manual to quit:
-        if (command == "QUIT") {
+        if (strcmp(buffer, "QUIT") == 0) {
             std::cout << "Received QUIT command. Stopping server." << std::endl;
             break;
         }
 
-        // Respond to UDP_client:
-        const char *response = "Data received.";
-        sendto(sockfd, response, strlen(response), 0, (const struct sockaddr *)&cliaddr, len);
+        // // Respond to UDP_client:
+        // const char *response = "Data received.";
+        // sendto(sockfd, response, strlen(response), 0, (const struct sockaddr *)&cliaddr, len);
     }
     
     close(sockfd);
